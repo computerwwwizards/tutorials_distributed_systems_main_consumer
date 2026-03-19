69130a48a84a0233c62d9694ba395e4fd14e49b3
shadow <shadow8095@gmail.com>
2026-03-19 13:28:49 -0500

    feat: router service

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { RouterProvider } from "react-router"
 import type { TopLevelCtx } from "./index.types"
-
+import { createRouter } from "./routes/__root"
+import { useMemo } from "react"
+import userAccesPlugin from  './routes/(global)/-config/user-acces-type/plugin'
+import { BasicContainer } from "@computerwwwizards/dependency-injection"
+import type { GlobalServices } from "./routes/(global)/-config/types"
+import routeServicePlugin from "./routes/(global)/-config/patch-routes/plugin"
 export interface AppProps{
   topLevelCtx?: TopLevelCtx
 }
 
 export default function App({}: AppProps){
-  return 'This is the main App'
+  const globalContainer = useMemo(()=>{
+    const container = new BasicContainer<GlobalServices>();
+
+    if(import.meta.env.PUBLIC_IS_GLOBAL_MOCK_ON)
+      container.useMocks()
+
+  container
+    .use(routeServicePlugin)
+    .use(userAccesPlugin)
+
+    return container
+
+  }, [])
+  return <RouterProvider router={createRouter(globalContainer)} />
 }
